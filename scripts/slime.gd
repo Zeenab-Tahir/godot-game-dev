a0a491d833c9c54c35c392e31caa96193de117da
Zeenab-Tahir <zainabtahirc@gmail.com>
2024-07-04 22:34:53 +0500

    modified enemy

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,7 +1,30 @@
 extends Node2D
+var direction=1
+const speed=50
+
+@onready var marzi =$AnimatedSprite2D
+@onready var rc_right = $RCRight
+@onready var rc_left = $RCLeft
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if rc_right.is_colliding():
+		marzi.flip_h=true
+		direction=-1
+		
+	if rc_left.is_colliding():
+		marzi.flip_h=false
+		direction=1
+	
+	
+	position.x+=direction*delta*speed
+	
+	
+	
+	
+	
+	
+	
+
