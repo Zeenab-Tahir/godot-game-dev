39934396702789e660643725f4563ae766700b46
Zeenab-Tahir <zainabtahirc@gmail.com>
2024-07-10 22:24:56 +0500

    added audio

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 @onready var game_manager = %game_manager
 
+@onready var animation_player = $AnimationPlayer
+
 func _on_body_entered(body):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
 	
+
 
